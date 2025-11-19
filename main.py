68d358586ed0f0c9238d3aabd4ef4b124b9f3db0
Tim O <timothywastaken@hotmail.com>
2025-11-19 17:45:15 +1100

    Fixed filename parsing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Notion MD file migrator/renamer
 
-Simple script that renames and copies the migrated Notion MD files by date in the format: YYYY-DD-MMTHHMM_Title.md
+Simple script that renames and copies migrated Notion MD files by date in the format: YYYY-DD-MMTHHMM_Title.md
 """
 
 from datetime import datetime
@@ -11,40 +11,67 @@ import os
 from pathlib import Path
 
 
-def file_search(old_folder_destination, new_folder_name):
-    os.makedirs(new_folder_name, exist_ok=True)
-
-    for filepath in Path(old_folder_destination).glob("*.md"):
-        parse_and_copy(filepath, new_folder_name)
-
-
-def parse_and_copy(filepath, new_folder_name):
+def parse(filepath):
     try:
         with open(filepath, "r") as f:
             lines = f.readlines()
+        if len(lines) < 3 or lines[2].split()[0] != "Created:":
+            raise ValueError(f"File {filepath} is in invalid MD format")
+
         date_line = lines[2]
         title_line = lines[0]
 
         date = date_line.split(":", 1)[1].strip()
-        dt = datetime.strptime(date, "%B %d, %Y %I:%M %p")
-        formatted_dt = dt.strftime("%Y-%m-%dT%H%M_")
+        dt_raw = datetime.strptime(date, "%B %d, %Y %I:%M %p")
+        dt = dt_raw.strftime("%Y-%m-%dT%H%M")
 
-        title_name = (
-            title_line.split("#", 1)[1].strip().replace(" ", "_").replace("/", "-")
-        )
+        title = title_line[2:].strip()
 
-        destination = new_folder_name + "/" + formatted_dt + title_name + ".md"
-        print(destination)
-        shutil.copy2(filepath, destination)
+        tags = ""
+        if (
+            len(lines) > 4
+            and len(lines[3].split()) > 1
+            and lines[3].split()[0] == "Tags:"
+        ):
+            tags_line = lines[3]
+            tags = tags_line.split(":", 1)[1].strip().replace(" ", "")
+
+        title = clean(title)
+        tags = clean(tags)
+        return dt, title, tags
 
     except Exception as e:
-        print(f"Error processing {filepath}: {e}")
+        raise ValueError(f"Error processing {filepath}: {e}")
+
+
+def clean(string):
+    safe_string = (
+        string.replace(" ", "_")
+        .replace("/", "-")
+        .replace(":", "-")
+        .replace("?", "")
+        .replace('"', "'")
+        .replace("<", "")
+        .replace(">", "")
+        .replace("|", "")
+        .replace("*", "")
+    )
+    return safe_string
 
 
 def main():
     old_folder_destination = input("Your folder's destination: ")
     new_folder_name = input("Name of new folder: ")
-    file_search(old_folder_destination, new_folder_name)
+    os.makedirs(new_folder_name, exist_ok=True)
+
+    for filepath in Path(old_folder_destination).glob("*.md"):
+        dt, title, tags = parse(filepath)
+        try:
+            new_filename = f"{new_folder_name}/{dt}_{title}_{tags}.md"
+            shutil.copy2(filepath, new_filename)
+            print(f"Successfully copied {new_filename}")
+        except Exception as e:
+            raise ValueError(f"Error copying file: {e}")
 
 
 if __name__ == "__main__":
